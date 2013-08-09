46ed5a4a8b1669b85aa0444d0bf1cd6e73b390b0
Matthias Buse <matthias.buse@coremedia.com>
2013-08-09 11:11:05 +0200

    Create quizzes.js
    This is just for testing...

---
+++ quizzes.js
@@ -0,0 +1,10 @@
+quizApp.quizzes = [
+    "data/youtube1.js",
+    "data/pictures1.js",
+    "data/pictures2.js",
+    "data/wordOrder1.js",
+    "data/wordOrder2.js",
+    "data/wordOrder3.js",
+    "data/multipleChoice1.js",
+    "data/multipleChoice2.js"
+];
