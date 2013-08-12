a076e8eccdcb5db213f495a6a8f93313d778c9b6
mbuse <matthias.buse@coremedia.com>
2013-08-12 19:03:18 +0200

    facebookTest: a test for FB.login(..). Displays all your friends...

---
+++ data/facebookTest.js
@@ -0,0 +1,9 @@
+quizApp.current.data = {
+  type: "facebookTest",
+  title: "Testing Facebook APIs",
+  appId: "142495904150",
+  locale: "en_US",
+  channelUrl: undefined,
+  status:true,
+  xfbml:true
+}
